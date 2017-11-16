5a92e3cce4494fbf74fcc2934a16629c1b1a3da3
Nursultan Kabylkas <kabylkas@gmail.com>
2017-11-15 20:04:52 -0800

    small fixes

--- rnn_tutorial/rnn.py
+++ rnn_tutorial/rnn.py
@@ -1,7 +1,10 @@
 import pandas as pd, numpy as np, tensorflow as tf
 import tweets
 
+#initialize the class
 t = tweets.tweets("../input/train.csv")
-for tok in t.tokenize("the war is coming! what you gonna do, bro? don't worry, bratan!"):
+
+#tokenize the string based on the data in train.csv
+for tok in t.tokenize("the war is coming! what you gonna do, bro? don't worry, brasdf!"):
   print(t.d[tok])
 
